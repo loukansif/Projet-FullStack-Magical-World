ff373bd6155e373c1525ace9d95dc565bd500d11
tandria <timothyandriano93@gmail.com>
2022-06-28 10:13:23 +0200

    "rename Home - fix incoming change app"

--- ExpressJS/client/src/App.js
+++ ExpressJS/client/src/App.js
@@ -3,13 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       
-=======
       <header className="App-header">
         
       </header>
->>>>>>> main
     </div>
   );
 }
